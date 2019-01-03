a8917c8dd0e72b8f032b09955ec17e4097841a9f
snowyswonderland <34998942+snowyswonderland@users.noreply.github.com>
2019-01-02 18:58:00 -0600

    Create raw.js

---
+++ events/raw.js
@@ -0,0 +1,16 @@
+const events = {
+    MESSAGE_REACTION_ADD: 'messageReactionAdd'
+};
+module.exports = async (client, event) => {
+  if (!events.hasOwnProperty(event.t)) return;
+  const { d: data } = event;
+const user = client.users.get(data.user_id);
+const channel = client.channels.get(data.channel_id)
+if (channel.messages.has(data.message_id)) return;
+
+const message = await channel.messages.fetch(data.message_id);
+  const emojiKey = data.emoji.name && data.emoji.id ? data.emoji.id : data.emoji.name
+  const reaction = message.reactions.find(r => r.emoji.name === emojiKey);
+  
+  client.emit(events[event.t], reaction, user);
+}
