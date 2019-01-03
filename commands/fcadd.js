8dbf966006af9b3d6c0261df6b08ba5f153cc7e0
snowyswonderland <34998942+snowyswonderland@users.noreply.github.com>
2019-01-02 19:47:36 -0600

    Update fcadd.js

--- commands/fcadd.js
+++ commands/fcadd.js
@@ -1,7 +1,12 @@
 exports.run = async (client, message, params) => {
+       client.fcs.ensure(message.author.id, {
+             3ds: '',
+              switch: ''
+              })
+       if (params[0].toLowerCase() === '3ds') {
        var friend = params.slice(0).join(' '),
            Discord = require('discord.js')
-        client.fcs.set(message.author.id, friend)
+        client.fcs.setProp(message.author.id, '3ds', friend)
 
         const embed = new Discord.RichEmbed()
          .setAuthor('SnowBuddy', client.user.avatarURL)
@@ -19,6 +24,24 @@ exports.run = async (client, message, params) => {
    message.member.addRole(role.id)
     message.channel.send('You are now able to request Pokemon!')
   } else return
+              }
+       else if (params[0].toLowerCase() === 'switch') {
+              var friend = params.slice(0).join(' ')
+              Discord = require('discord.js')
+              
+              client.fcs.setProp(message.author.id, 'switch', friend)
+
+        const embed = new Discord.RichEmbed()
+         .setAuthor('SnowBuddy', client.user.avatarURL)
+      .setColor("RANDOM")
+      .setThumbnail(message.author.avatarURL)
+      .setDescription(friend)
+      .setFooter("Added by " + message.author.username, message.author.avatarURL)
+        .setTimestamp()
+        client.channels.get("451676070689636355").send(embed)
+        message.channel.send('Successfully set your FC!')
+    
+              }
 }
 
 exports.conf = {
