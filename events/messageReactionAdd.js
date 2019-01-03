4d93d14359dee8a989961e057f45b842c931c427
snowyswonderland <34998942+snowyswonderland@users.noreply.github.com>
2019-01-02 19:10:20 -0600

    Create messageReactionAdd.js

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,13 @@
+module.exports = (client, reaction, user) => {
+if (reaction.message.channel.id !== '456937038659321856') return
+const user = client.users.get(client.requests.getProp(reaction.message.id, 'requester'))
+if (reaction.emoji.name === '✨') {
+client.requests.setProp(reaction.message.id, "status", "DBNT")
+}
+if (reaction.emoji.name === '❌') {
+client.requests.setProp(reaction.message.id, "status", "ILLEGAL")
+}
+if (reaction.emoji.name = 'aacheck') {
+client.requests.setProp(reaction.message.id, "status", "TRADED")
+}
+}
