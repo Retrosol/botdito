a763001025540981cdf34220aeedd4a463f6c0f1
snowyswonderland <34998942+snowyswonderland@users.noreply.github.com>
2019-01-15 11:36:05 -0600

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,14 +3,14 @@ if (reaction.message.channel.id !== '456937038659321856') return
 const auser = client.users.get(client.requests.getProp(reaction.message.id, 'requester'))
 if (reaction.emoji.name === '✨') {
 client.requests.setProp(reaction.message.id, "status", "DBNT")
-console.log(client.requests.getProp(reaction.message.id, 'status'))
+return console.log(client.requests.getProp(reaction.message.id, 'status'))
 }
 if (reaction.emoji.name === '❌') {
 client.requests.setProp(reaction.message.id, "status", "ILLEGAL")
-  console.log(client.requests.getProp(reaction.message.id, 'status'))
+return console.log(client.requests.getProp(reaction.message.id, 'status'))
 }
 if (reaction.emoji.name = 'aacheck') {
 client.requests.setProp(reaction.message.id, "status", "TRADED")
-  console.log(client.requests.getProp(reaction.message.id, 'status'))
+  return console.log(client.requests.getProp(reaction.message.id, 'status'))
 }
 }
