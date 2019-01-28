b496d89f9ef08298cdd4d2035861c5fc46e819b3
Ryan Munro (Monbrey) <rsm999@uowmail.edu.au>
2019-01-28 13:37:39 +1100

    Bugs fixed

--- commands/help.js
+++ commands/help.js
@@ -1,39 +1,40 @@
- exports.run = async (client, message, params) => {
-  const Discord = require('discord.js'),
-        cmd = params[0]
-  
- if (!cmd) {
-   const embed = new Discord.RichEmbed()
-   .setTitle('Here is a list of commands!')
-   .setColor("RANDOM")
-   .setTimestamp()
-   .setDescription('``' + client.commands.map(m => m.help.name).join(', ').replace(', dm', '') + '``')
-   .setFooter('For more info on a command, do !help (command name)')
-   message.channel.send(embed)
- } else {
-  if (client.commands.has(cmd)) {
-    const props = require(`../commands/${cmd}.js`)
-   const embed = new Discord.RichEmbed()
-   .setTitle(props.help.name)
-   .setColor("RANDOM")
-   .setTimestamp()
-   .addField('Description', props.help.description)
-   .addField('Usage', props.help.usage)
-   .addField('Aliases', props.conf.aliases.length > 0 ? props.conf.aliases : "None")
-   .setFooter('[] = Required | () = Optional')
-   message.channel.send(embed)
-  } else return message.channel.send('``Could not find command ' + cmd + '``')
- }
+const { RichEmbed } = require("discord.js")
+
+exports.run = async (client, message, params) => {
+    const cmd = params[0]
+
+    if (!cmd) {
+        const embed = new RichEmbed()
+            .setTitle("Here is a list of commands!")
+            .setColor("RANDOM")
+            .setTimestamp()
+            .setDescription("``" + client.commands.map(m => m.help.name).join(", ").replace(", dm", "") + "``")
+            .setFooter("For more info on a command, do !help (command name)")
+        message.channel.send(embed)
+    } else {
+        if (client.commands.has(cmd)) {
+            const props = require(`../commands/${cmd}.js`)
+            const embed = new RichEmbed()
+                .setTitle(props.help.name)
+                .setColor("RANDOM")
+                .setTimestamp()
+                .addField("Description", props.help.description)
+                .addField("Usage", props.help.usage)
+                .addField("Aliases", props.conf.aliases.length > 0 ? props.conf.aliases : "None")
+                .setFooter("[] = Required | () = Optional")
+            message.channel.send(embed)
+        } else return message.channel.send("``Could not find command " + cmd + "``")
+    }
 }
 
 exports.conf = {
-  aliases: ['cmd'],
-  permLevel: 0,
-  nsfw: false
+    aliases: ["cmd"],
+    permLevel: 0,
+    nsfw: false
 }
 
 exports.help = {
-  name: "help",
-  description: "Shows info about a command",
-  usage: "+help (command name)"
+    name: "help",
+    description: "Shows info about a command",
+    usage: "+help (command name)"
 }
--- commands/myreqs.js
+++ commands/myreqs.js
@@ -1,29 +1,30 @@
-exports.run = async (client, message, params) => {
-  const { RichEmbed } = require('discord.js'),
-  statuses = {
-  "ND": 'Not Done',
-  'DBNT': 'Done but not traded',
-  'TRADED': 'Done and traded',
-  'ILLEGAL': 'Denied because request was illegal.'
-  }
-const embed = new RichEmbed()
-.setTitle('Your Requests')
-.setColor('RANDOM')
+const { RichEmbed } = require("discord.js")
 
-const reqs = client.requests.findAll('requester', message.author.id)
-console.log(reqs)
-embed.setDescription(reqs.map((m, k) => `**${k}** - ${statuses[m.status]}`).join('\n'))
-message.channel.send(embed)
+exports.run = async (client, message, params) => {
+    const statuses = {
+        "ND": "Not Done",
+        "DBNT": "Done but not traded",
+        "TRADED": "Done and traded",
+        "ILLEGAL": "Denied because request was illegal."
+    }
+    const embed = new RichEmbed()
+        .setTitle("Your Requests")
+        .setColor("RANDOM")
+
+    const reqs = client.requests.findAll("requester", message.author.id)
+    console.log(reqs)
+    embed.setDescription(reqs.map((m, k) => `**${k}** - ${statuses[m.status]}`).join("\n"))
+    message.channel.send(embed)
 }
 
 exports.conf = {
-  aliases: [],
-  permLevel: 0,
-  nsfw: false
+    aliases: [],
+    permLevel: 0,
+    nsfw: false
 }
 
 exports.help = {
-  name: "myreqs",
-  description: "Look at the status of your requests",
-  usage: "!myreqs"
+    name: "myreqs",
+    description: "Look at the status of your requests",
+    usage: "!myreqs"
 }
