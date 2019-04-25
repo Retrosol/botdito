b55b01831a3cd4c05386e19d91834a641b3eeead
unknxnwn <34998942+xunknxwnx@users.noreply.github.com>
2019-04-25 13:25:22 -0500

    Update dex.js

--- commands/dex.js
+++ commands/dex.js
@@ -1,5 +1,25 @@
 exports.run = async (client, message, params) => {
+  const r = require('request'),
+        pokemon = params.slice(0).join('_').toLowerCase(),
+        embedColours = {
+          Red: 16724530,
+          Blue: 2456831,
+          Yellow: 16773977,
+          Green: 4128590,
+          Black: 3289650,
+          Brown: 10702874,
+          Purple: 10894824,
+          Gray: 9868950,
+          White: 14803425,
+          Pink: 16737701
+        },
+        pokemon_url = params.slice(0).join('-').toLowerCase()
   
+  let poke_info
+  
+  r(`https://github.com/jalyna/oakdex-pokedex/blob/master/data/pokemon/${pokemon}.json`, (err, res, body) => {
+    console.log(err, res, body)
+  })
 }
 
 exports.conf = {
