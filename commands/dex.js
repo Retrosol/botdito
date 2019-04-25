97e21fa2f2c9335a836de3e4901768ad9b9d8a13
unknxnwn <34998942+xunknxwnx@users.noreply.github.com>
2019-04-25 13:16:52 -0500

    Create dex.js

---
+++ commands/dex.js
@@ -0,0 +1,15 @@
+exports.run = async (client, message, params) => {
+  
+}
+
+exports.conf = {
+  aliases: [],
+  permLevel: 0,
+  nsfw: false
+}
+
+exports.help = {
+  name: "dex",
+  description: "A Pokèdex... in Discord!",
+  usage: "+dex <pokemon>"
+}
